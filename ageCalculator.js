5dfbe487b44635ab0a6fd384d2ade727e6f703ef
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-01-20 02:38:23 +0000

    created new file ageCalculator.js

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  var age = currentYear - yearOfBirth;
+  return (name + " is " + age + " years old.");  
+}
+
+console.log(ageCalculator("Suzie", 1983, 2015));
