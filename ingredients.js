fa76aec2f3d186960c6b3343a33ab08a6fe85675
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-01-20 04:05:16 +0000

    for loop, while loop, reverse iterate

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+i=0;
+while (i <=7) {
+  console.log(ingredients[i]);
+    i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+let x;
+for (x = 0; x <= 7; x++) {
+  console.log(ingredients[x]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+let y;
+for (y = 7; y >=0; y--){
+    console.log(ingredients[y]);
+}
